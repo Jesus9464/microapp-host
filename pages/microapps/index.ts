96f900d7b520d015e572d4717ccad2f79b008be8
Jesus Galvis <74638215+Jesus9464@users.noreply.github.com>
2024-07-17 19:49:26 -0500

    feat: update typescript

--- pages/microapps/index.ts
+++ pages/microapps/index.ts
@@ -1,15 +1,18 @@
 import dynamic from "next/dynamic";
 
+//@ts-ignore
 export const Microapp1 = dynamic(() => import("microapp-1/microapp1"), {
   ssr: false,
   suspense: true,
 });
 
+//@ts-ignore
 export const Microapp2 = dynamic(() => import("microapp-2/microapp2"), {
   ssr: false,
   suspense: true,
 });
 
+//@ts-ignore
 export const Microapp3 = dynamic(() => import("microapp-3/microapp3"), {
   ssr: false,
   suspense: true,
