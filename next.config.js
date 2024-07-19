9bff9a9c252175f7a6cd114877e0d5bc960d699d
Jesus Galvis <74638215+Jesus9464@users.noreply.github.com>
2024-07-18 20:05:15 -0500

    fix: update microapp host

--- next.config.js
+++ next.config.js
@@ -10,17 +10,17 @@ const nextConfig = {
         filename: "static/chunks/remoteEntry.js",
         remotes: {
           "microapp-1": `microapp-1@${
-            process.env.NODE_ENV === "production"
+            process.env.NODE_ENV === "development"
               ? "http://localhost:3001/"
               : "https://microapp1-jesus9464s-projects.vercel.app/"
           }_next/static/chunks/remoteEntry.js`,
           "microapp-2": `microapp-2@${
-            process.env.NODE_ENV === "production"
+            process.env.NODE_ENV === "development"
               ? "http://localhost:3001"
               : "https://microapp2-jesus9464s-projects.vercel.app/"
           }_next/static/chunks/remoteEntry.js`,
           "microapp-3": `microapp-3@${
-            process.env.NODE_ENV === "production"
+            process.env.NODE_ENV === "development"
               ? "http://localhost:3002/"
               : "https://microapp3-jesus9464s-projects.vercel.app/"
           }_next/static/chunks/remoteEntry.js`,
